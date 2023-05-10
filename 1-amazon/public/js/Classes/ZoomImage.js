62b8b050c6714e98b31a343583d17cff21bf8027
anderson <andersonlscesar@gmail.com>
2023-05-10 14:06:23 -0300

    lens following the cursor

--- 1-amazon/public/js/Classes/ZoomImage.js
+++ 1-amazon/public/js/Classes/ZoomImage.js
@@ -7,6 +7,7 @@ export class ZoomImage {
     manageEvents() {
         this._image.addEventListener('mouseenter', this._onMouseEnter.bind(this));
         this._image.addEventListener('mouseleave', this._onMouseLeave.bind(this));
+        this._image.addEventListener('mousemove', this._onMouseMove.bind(this));
     }
     /**
      * it is actived when the user over the main image
@@ -14,6 +15,7 @@ export class ZoomImage {
      */
     _onMouseEnter(e) {
         this._createLens();
+        this._setLensPosition(e);
     }
     /**
      * It is actived when the cursor leaves the main image
@@ -22,10 +24,27 @@ export class ZoomImage {
         var _a;
         (_a = this._lens) === null || _a === void 0 ? void 0 : _a.remove();
     }
+    _onMouseMove(e) {
+        this._setLensPosition(e);
+    }
+    /**
+     * This function creates the lens
+     */
     _createLens() {
         var _a;
         this._lens = document.createElement('div');
         this._lens.classList.add('lens');
         (_a = this._image.parentElement) === null || _a === void 0 ? void 0 : _a.appendChild(this._lens);
+    }
+    /**
+     * It will collect and set the x and y coordinates from the cursor pointer
+     * and it sets the lens's position
+     * @param { MouseEvent } e
+     */
+    _setLensPosition(e) {
+        const { offsetX, offsetY } = e;
+        const { height, width } = this._lens.getBoundingClientRect();
+        this._lens.style.top = `${offsetY - height / 2}px`;
+        this._lens.style.left = `${offsetX - width / 2}px`;
     }
 }
--- 1-amazon/public/ts/Classes/ZoomImage.ts
+++ 1-amazon/public/ts/Classes/ZoomImage.ts
@@ -15,6 +15,7 @@ export class ZoomImage
     {
         this._image.addEventListener( 'mouseenter', this._onMouseEnter.bind( this ) );
         this._image.addEventListener( 'mouseleave', this._onMouseLeave.bind( this ) );
+        this._image.addEventListener( 'mousemove', this._onMouseMove.bind( this ) );
     }
 
 
@@ -26,6 +27,7 @@ export class ZoomImage
     private _onMouseEnter( e: MouseEvent ): void 
     {
         this._createLens();
+        this._setLensPosition( e );
     }   
 
     /**
@@ -38,10 +40,34 @@ export class ZoomImage
     }
 
 
+    private _onMouseMove( e: MouseEvent ): void 
+    {
+        this._setLensPosition( e );
+    }
+
+    /**
+     * This function creates the lens
+     */
+
     private _createLens(): void 
     {
         this._lens = document.createElement( 'div' );
         this._lens.classList.add( 'lens' );
         this._image.parentElement?.appendChild( this._lens );
     }
+
+    /**
+     * It will collect and set the x and y coordinates from the cursor pointer
+     * and it sets the lens's position
+     * @param { MouseEvent } e 
+     */
+
+    private _setLensPosition( e: MouseEvent ): void 
+    {
+        const { offsetX, offsetY } = e;
+        const { height, width } = this._lens.getBoundingClientRect();
+        this._lens.style.top = `${ offsetY - height / 2 }px`;
+        this._lens.style.left = `${ offsetX - width / 2 }px`;        
+    }
+
 }
