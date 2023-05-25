c9f2b6f51acf6c50b781298d78d6be8915286e95
anderson <andersonlscesar@gmail.com>
2023-05-25 09:21:24 -0300

    Modal
    Now you can click on the images and it will open a modal and inside that you will see the selected image

--- 1-amazon/public/js/Classes/Modal.js
+++ 1-amazon/public/js/Classes/Modal.js
@@ -2,15 +2,27 @@ export class Modal {
     constructor(_images, _modal) {
         this._images = _images;
         this._modal = _modal;
+        this._btnCloseModal = document.querySelector('.close-modal');
         this._images = _images;
         this._modal = _modal;
         this._manageEvents();
     }
     _manageEvents() {
         this._images.forEach((img) => {
-            img.addEventListener('mouseenter', this._onMouseEenter.bind(this, img));
+            img.addEventListener('click', this._openModal.bind(this, img));
         });
+        this._btnCloseModal.addEventListener('click', this._closeModal.bind(this));
     }
-    _onMouseEenter(img) {
+    _openModal(img) {
+        this._modal.modalContainer.classList.add('modal--active');
+        let i = img;
+        this._setImage(i);
+    }
+    _closeModal() {
+        this._modal.modalContainer.classList.remove('modal--active');
+    }
+    _setImage(img) {
+        let i = img;
+        this._modal.modalContent.children[this._modal.modalContent.children.length - 1].setAttribute('src', `${i.src}`);
     }
 }
--- 1-amazon/public/ts/Classes/Modal.ts
+++ 1-amazon/public/ts/Classes/Modal.ts
@@ -5,6 +5,8 @@ type ModalInfo<U, I> = {
 
 export class Modal<T extends Node>
 {
+    private _btnCloseModal = document.querySelector( '.close-modal' ) as HTMLElement;
+
     constructor
     (
         private _images: NodeListOf<T>,
@@ -19,14 +21,26 @@ export class Modal<T extends Node>
     private _manageEvents(): void 
     {
         this._images.forEach( ( img ) => {
-            img.addEventListener( 'mouseenter', this._onMouseEenter.bind( this, img ) );
+            img.addEventListener( 'click', this._openModal.bind( this, img ) );
         } );
+        this._btnCloseModal.addEventListener( 'click', this._closeModal.bind( this ) );
     }   
 
+    private _openModal( img: T ): void 
+    {
+        this._modal.modalContainer.classList.add( 'modal--active' );
+        let i = <unknown> img;
+        this._setImage( i );
+    }
 
-    private _onMouseEenter( img: T ): void 
+    private _closeModal(): void 
+    {
+        this._modal.modalContainer.classList.remove( 'modal--active' );
+    }
+
+    private _setImage( img: unknown ): void 
     {        
-
-        
+        let i = <HTMLImageElement> img;
+        this._modal.modalContent.children[ this._modal.modalContent.children.length - 1 ].setAttribute( 'src', `${ i.src }` );
     }
 }
