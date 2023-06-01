505cd4ecf3bd3b004a37a76d40c7d43e5654d351
anderson <andersonlscesar@gmail.com>
2023-06-01 10:08:48 -0300

    Consumo de API finalizado

--- 2_wikipedia/public/js/MyWiki.js
+++ 2_wikipedia/public/js/MyWiki.js
@@ -1,20 +1,56 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class MyWiki {
     constructor(input) {
         this.input = input;
-        this.url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch';
+        this.url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch=';
         this.config = {
             method: 'GET',
             headers: {
                 'Content-type': 'application/json'
             }
         };
+        this.result = document.querySelector('#results');
         this.manageEvents();
     }
     manageEvents() {
-        this.input.addEventListener('input', this.loadData.bind(this));
+        this.input.addEventListener('input', this.getData.bind(this));
     }
-    loadData() {
+    getData() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.result.innerHTML = 'Carregando ...';
+            let dataSearched = this.url + this.input.value;
+            try {
+                const fetchData = yield fetch(dataSearched, this.config);
+                const json = yield fetchData.json();
+                this.render(json.query.search);
+            }
+            catch (error) {
+                this.result.innerHTML = 'Nada a pesquisar ...';
+            }
+        });
+    }
+    render(data) {
+        let html = '';
+        console.log(data);
+        data.forEach(data => {
+            html += `
+                        <div class="results__card">
+                            <h1>${data.title}</h1>
+                            <p>${data.snippet}</p>
+                        </div>
+    
+            `;
+        });
+        this.result.innerHTML = html;
     }
 }
 const input = document.querySelector('.search__input');
--- 2_wikipedia/public/ts/MyWiki.ts
+++ 2_wikipedia/public/ts/MyWiki.ts
@@ -1,18 +1,19 @@
-interface Elemetns {
-    input: HTMLInputElement,    
+interface JSONData {
+    [key: string | number ]: string | number
 }
-
 
 class MyWiki 
 {
-    public readonly url: string = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch';
-    
+    public readonly url: string = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch=';
+
     public readonly config = {
         method: 'GET',
         headers: {
             'Content-type': 'application/json'
         }
     };
+
+    private result = document.querySelector( '#results' ) as HTMLDivElement;
 
     constructor
     (
@@ -24,15 +25,43 @@ class MyWiki
 
     private manageEvents(): void 
     {
-        this.input.addEventListener( 'input', this.loadData.bind( this ) );
+        this.input.addEventListener( 'input', this.getData.bind( this ) );
     }
 
 
-    private loadData()
+    private async getData(): Promise<void>
+    {   
+        this.result.innerHTML = 'Carregando ...';
+        let dataSearched = this.url + this.input.value;
+        try {
+
+            const fetchData: Response = await fetch( dataSearched, this.config );
+            const json: any = await fetchData.json();
+            this.render( json.query.search );
+
+        } catch ( error ) { 
+            this.result.innerHTML = 'Nada a pesquisar ...';
+        }
+    }
+
+
+    private render( data: Array<JSONData> ): void
     {
-        
-    }
+        let html = '';
+        console.log(data)
+        data.forEach( data => {
+            html += `
+                        <div class="results__card">
+                            <h1>${ data.title }</h1>
+                            <p>${ data.snippet }</p>
+                        </div>
+    
+            `;
+        });
+ 
+        this.result.innerHTML = html;
 
+    }
 }
 
 const input = document.querySelector( '.search__input' ) as HTMLInputElement;
