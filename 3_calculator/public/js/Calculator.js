a16ef254a70b4814937aa05bc20da308dd62e47a
anderson <andersonlscesar@gmail.com>
2023-06-29 10:51:45 -0300

    Função observerChanges && funçao formatNumber
    Função observerChanges coleta os childNodes do elemento responsável pelo display na calculadora.
    Função formatNumber recebe o valor do childNode e o altera de acordo com a quantidade de caracteres (dezenas, milhares ...)

--- 3_calculator/public/js/Calculator.js
+++ 3_calculator/public/js/Calculator.js
@@ -30,10 +30,45 @@ class Calculator {
     display(e) {
         const btn = e.target;
         this.displayCurrent.innerHTML += btn.value;
+        this.observeChanges();
     }
+    /************************************************************************************************************
+    *
+    *  adiciona as operações dentro de um span para que seja possível adicionar um style próprio para o sinal.
+    *
+    *  @param {MouseEvent} e
+    *
+    ************************************************************************************************************/
     addOperation(e) {
         const btn = e.target;
-        console.log(this.displayCurrent.innerHTML);
+        const operator = this.createSpan();
+        operator.classList.add('operator');
+        operator.textContent = btn.value;
+        this.displayCurrent.appendChild(operator);
+        this.observeChanges();
+    }
+    /*********************************************************************************************
+    *
+    *  Essa função coleta os childNodes para que seja possível formatar cada expressão númerica
+    * Exemplo:
+    * 9 + 9
+    * 9 é um childNode do tipo 3 => teremos mais facilidade em coletar apenas as expressões sem sinai
+    * Cada expressão é constituida por um <span></span>, este childNode é do tipo 1
+    * Assim conseguiremos diferenciar os elementos
+    *
+    *********************************************************************************************/
+    observeChanges() {
+        const childNodes = this.displayCurrent.childNodes;
+        childNodes.forEach(child => {
+            if (child.nodeType === 3)
+                child.nodeValue = this.formatNumber(child.nodeValue);
+        });
+    }
+    formatNumber(value) {
+        value = value.replace(/\./g, '');
+        if (value.length > 3 && !value.includes(','))
+            value = value.replace(/(\d)(?=(\d{3})+$)/g, '$1.');
+        return value;
     }
     /*******************************
     *
--- 3_calculator/public/ts/Calculator.ts
+++ 3_calculator/public/ts/Calculator.ts
@@ -38,15 +38,56 @@ class Calculator
     private display(e: MouseEvent)
     {
         const btn = e.target as HTMLButtonElement;
-        this.displayCurrent.innerHTML += btn.value;       
+        this.displayCurrent.innerHTML += btn.value;    
+        this.observeChanges();
+
     }
+
+    /************************************************************************************************************
+    *
+    *  adiciona as operações dentro de um span para que seja possível adicionar um style próprio para o sinal.
+    *                                                                                                         
+    *  @param {MouseEvent} e                                                                                  
+    *
+    ************************************************************************************************************/
 
 
     private addOperation(e: MouseEvent)
     {
         const btn = e.target as HTMLButtonElement;
-        
-        console.log(this.displayCurrent.innerHTML);
+        const operator = this.createSpan();
+        operator.classList.add('operator');
+        operator.textContent = btn.value;
+        this.displayCurrent.appendChild(operator);
+        this.observeChanges();
+    }
+
+    /*********************************************************************************************
+    *
+    *  Essa função coleta os childNodes para que seja possível formatar cada expressão númerica
+    * Exemplo:
+    * 9 + 9
+    * 9 é um childNode do tipo 3 => teremos mais facilidade em coletar apenas as expressões sem sinai
+    * Cada expressão é constituida por um <span></span>, este childNode é do tipo 1
+    * Assim conseguiremos diferenciar os elementos 
+    *
+    *********************************************************************************************/
+
+
+    private observeChanges(): void 
+    {
+        const childNodes: NodeListOf<ChildNode> = this.displayCurrent.childNodes;
+        childNodes.forEach(child => {
+            if (child.nodeType === 3) child.nodeValue = this.formatNumber(child.nodeValue as string);
+        });
+    }
+
+
+    private formatNumber(value: string): string
+    {
+        value = value.replace(/\./g, '');
+        if (value.length > 3 && !value.includes(',')) value = value.replace(/(\d)(?=(\d{3})+$)/g, '$1.');
+        return value;
     }
     
 
